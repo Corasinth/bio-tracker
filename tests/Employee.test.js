a98b36a254d5896d99c28582ff275feca5cd00ee
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-22 00:18:41 -0500

    add tests for employee methods

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -6,9 +6,29 @@ describe ('employee', () => {
             const employee = new Employee ('Bob', 1, 'email@email');
             expect(employee.name).toEqual('Bob');
             expect(employee.id).toEqual(1);
-            expect(employee.email).toEqual('email@email');
+            expect(employee.email).toEqual(1);
         })
     })
+    describe ('getName', () => {
+       it ('should return the employees name', () => {
+        expect(employee.getName()).toBe(employee.name);
+       });
+    }),
+    
+    describe ('getId', () => {
+       it ('should return the employees id', () => {
+        expect(employee.getEmail()).toBe(employee.id);
+       });
+    }),
+    
+    describe ('getEmail', () => {
+       it ('should return the emloyees email', () => {
+        expect(employee.getEmail()).toBe(employee.email);
+       });
+    }),
+     describe ('getRole', () => {
+       it ('should return empployee', () => {
+        expect(employee.getRole()).toBe(employee.constructor.name);
+       });
+    });
 })
-
-
